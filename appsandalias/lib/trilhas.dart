4fc49c56070f18eb6102417e2eb33b34bece355c
Leonardo Pessanha <143332467+Gudinez08@users.noreply.github.com>
2024-09-11 20:54:34 -0300

    criando tela de trilhas

---
+++ appsandalias/lib/trilhas.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class TrilhasPag extends StatelessWidget {
+const TrilhasPag({super.key}); 
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
+        ),
+      )
+    );
+
+
+}
+
+}
