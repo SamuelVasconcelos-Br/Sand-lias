a5c00a148e0e0bd4fb1253fb4961031667996aac
Leonardo Pessanha <143332467+Gudinez08@users.noreply.github.com>
2024-09-18 21:07:53 -0300

    editando

--- appsandalias/lib/praias.dart
+++ appsandalias/lib/praias.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      title: 'Praias',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
